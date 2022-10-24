5ae5f086feb1a521154b7a25273f7c3184f2abeb
Gu Chao <guchao2000@163.com>
2022-10-24 23:22:31 +0800

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,30 @@
 // client side javascript to call back end api to
-console.log("Client side javascript file is loaded!");
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector('#message-2')
-
-
-weatherForm.addEventListener('submit',(e)=>{
+weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  messageOne.textContent = 'Loading ...'
-  messageTwo.textContent = ''
+  messageOne.textContent = "Loading ...";
+  messageTwo.textContent = "";
 
   const location = search.value;
-  const url = 'http://127.0.0.1:3000/weather?address='+location;
-  console.log (url);
+  const url = "http://127.0.0.1:3000/weather?address=" + location;
+  console.log(url);
   fetch(url).then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-      messageOne.textContent = data.error
-
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-      messageOne.textContent = data.location 
-      messageTwo.textContent = data.forecast
-    }
+    response.json().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+        messageOne.textContent = data.error;
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      }
+    });
   });
-});
 
   console.log(location);
-})
+});
